e1090f586c7854a8515cde58f33aae0f32b73d1a
TheRanger22 <schandingl1@nku.edu>
2021-06-16 15:41:39 -0400

    Added a test suite, laid foundation for ExcelMerger.cs

---
+++ EMTest/Tests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using ExcelMerge;
+using NUnit.Framework;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace EMTest {
+    [TestFixture]
+    public class Tests {
+        ExcelMerger _em = new ExcelMerger();
+
+        [Test]
+        public void Test1() {
+            var excelApp = new Excel.Application();
+            excelApp.Visible = true;
+            Excel._Workbook wb = excelApp.Workbooks.Add();
+            Excel._Worksheet workSheet = (Excel.Worksheet) wb.ActiveSheet;
+            workSheet.Cells[1, "A"] = "ID Number";
+            workSheet.Cells[1, "B"] = "Current Balance";
+            var row = 1;
+            Tuple<string, int>[] accounts = new[] {
+                new Tuple<string, int>("ALPHA", 1),
+                new Tuple<string, int>("BETA", 2),
+                new Tuple<string, int>("GAMMA", 3)
+            };
+            foreach (var acct in accounts) {
+                row++;
+                workSheet.Cells[row, "A"] = acct.Item1;
+                workSheet.Cells[row, "B"] = acct.Item2;
+            }
+
+            wb.SaveAs("TEST");
+            wb.Close();
+            excelApp.Quit();
+            Assert.True(true);
+        }
+
+        [Test]
+        public void Test2() {
+            var excelApp = new Excel.Application();
+            excelApp.Visible = false;
+            Excel._Workbook wb = excelApp.Workbooks.Open("C:\\Users\\Basestar\\Documents\\TEST.xlsx");
+            Excel._Worksheet ws = (Excel._Worksheet) wb.ActiveSheet;
+            var obj = (string)(ws.Cells[1, 1] as Excel.Range).Value;
+            wb.Close();
+            wb = excelApp.Workbooks.Add();
+            Excel._Worksheet workSheet = (Excel.Worksheet) wb.ActiveSheet;
+            workSheet.Cells[1, "A"] = obj;
+            
+            wb.SaveAs("TEST2");
+            wb.Close();
+            excelApp.Quit();
+            
+        }
+        
+    }
+}
--- ExcelMerger.cs
+++ ExcelMerger.cs
@@ -1,7 +1,67 @@
-﻿namespace ExcelMerge
-{
-    public class ExcelMerger
-    {
-        
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelMerge {
+    public class ExcelMerger {
+        public string Filename { get; set; }
+        private HashSet<string> _files;
+        private Excel._Application _app;
+        private Excel._Workbook _workbook;
+        private Excel._Worksheet _worksheet;
+
+
+        public ExcelMerger() {
+            Filename = $"untitled - {DateTime.Today}";
+            _files = new HashSet<string>();
+            _app = new Excel.Application();
+            _workbook = _app.Workbooks.Add();
+            _worksheet = (Excel.Worksheet) _workbook.ActiveSheet;
+        }
+
+        public void AddWorkbook(string filename) {
+            _files.Add(filename);
+        }
+
+        public void AddWorkbooks(string[] filenames) {
+            foreach (string filename in filenames) {
+                AddWorkbook(filename);
+            }
+        }
+
+        public void Merge() {
+            
+            
+            var enumerator = _files.GetEnumerator();
+            var tempWorkbook = _app.Workbooks.Open(enumerator.Current, ReadOnly: true);
+            var tempWorksheet = (Excel._Worksheet) tempWorkbook.ActiveSheet;
+            
+
+            /* Read the first row headers of the first file */
+
+            /* Match the contents of all subsequent files to the headers pulled from the first file */
+        }
+
+        private string _itemSelect(int row, int column) {
+            
+            return null;
+        }
+
+        public void Export() {
+            _workbook.SaveAs(Filename);
+            
+        }
+
+        public void ExportAs(string filename) {
+            Filename = filename;
+            Export();
+        }
+
+        public void Quit() {
+            _app.Quit();
+        }
+
+        private void _readFirstRow() { }
     }
 }
